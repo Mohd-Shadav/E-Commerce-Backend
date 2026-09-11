fac9d92f9e8f635de9dbd6d599003efa4a925746
Mohd. Shadav <muhammadshadav166@gmail.com>
2026-09-12 01:52:32 +0530

    origin

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = async ()=>{
-    try{
+const connectDB = async () => {
+    try {
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("Database Connected successfully!!!");
-    }catch(err)
-    {
-        console.log("Internal Server Error - Database were not connected.");
+
+        console.log("✅ Database Connected Successfully!!!");
+
+    } catch (err) {
+
+        console.error("❌ Database Connection Error:");
+        console.error(err);
+
     }
-}
+};
 
 module.exports = connectDB;
